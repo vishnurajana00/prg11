6c32f7a562fcdf376aa26300bc075024783e764c
vishnurajana00 <vishnurajana00@gmail.com>
2026-09-23 11:18:24 +0530

    Implement login feature with conditional greeting
    Added login functionality with state management and conditional rendering.

--- src/App.js
+++ src/App.js
@@ -1,26 +1,33 @@
-import { useState } from "react";
+ import React, { useRef, useState } from "react";
 import "./App.css";
-
 function App() {
+  const [name, setName] = useState("");
+  const [loggedIn, setLoggedIn] = useState(false);
+  const inputRef = useRef();
+  const handleLogin = () => {
+    setName(inputRef.current.value);
+    setLoggedIn(true);
+  };
 
-  // TODO 1:
-  // Create a state variable named isLoggedIn.
-  // Initial value should be false.
-
-
-  // TODO 2:
-  // Use a ternary operator to display:
-  //
-  // If logged in:
-  //     Welcome Student
-  //
-  // Otherwise:
-  //     Login button
-  //
-  // The Login button should use onClick
-  // to change the login state.
-
-
+  return (
+    <div className="title">
+      {loggedIn ? (
+        <h1>Welcome Student {name}</h1>
+      ) : (
+        <>
+          <input
+            type="text"
+            ref={inputRef}
+            placeholder="Enter your name"
+          />
+          <button onClick={handleLogin}>
+            Login
+          </button>
+        </>
+      )}
+    </div>
+   
+  );
 }
 
 export default App;
